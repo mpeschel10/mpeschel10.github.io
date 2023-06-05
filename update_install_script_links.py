39a3d5db12bdec2a058c5536fab9eeb64f36cb8a
Mark Peschel <mpeschel10@gmail.com>
2023-06-05 13:44:50 -0400

    Fix I moved download generator; generate download script

--- update_install_script_links.py
+++ update_install_script_links.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 script_dir = Path(__file__).resolve().parent
-site_dir = script_dir.joinpath('mpeschel10.github.io')
-install_scripts_dir = site_dir.joinpath('arch-install-scripts')
-link_path = site_dir.joinpath('00_download_install_scripts.sh')
+install_scripts_dir = script_dir.joinpath('arch-install-scripts')
+link_path = script_dir.joinpath('00_download_install_scripts.sh')
 
 url_base = 'https://mpeschel10.github.io/'
-links = [url_base + str(path.relative_to(site_dir)) for
+links = [url_base + str(path.relative_to(script_dir)) for
     path in install_scripts_dir.iterdir()]
 
 with open(link_path, 'w') as link_file:
